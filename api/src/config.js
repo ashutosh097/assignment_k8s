eeca655c880012651e3d27ce24cb0cff55670fd7
Ashutosh Kumar <ashutoshkumar@Ashutoshs-MacBook-Pro.local>
2026-06-23 13:57:42 +0530

    fix the port issue

--- api/src/config.js
+++ api/src/config.js
@@ -12,10 +12,10 @@ if (missingEnv.length > 0) {
 
 module.exports = {
   host: process.env.HOST || '0.0.0.0',
-  port: Number(process.env.PORT),
+  port: Number(process.env.PORT) || 3000,
   db: {
     host: process.env.DB_HOST ,
-    port: Number(process.env.DB_PORT),
+    port: Number(process.env.DB_PORT) || 3306,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
